8a62168c0dc5c4f6e6db70cff275356d6c82631b
Mahmoud Allama <mahmoudallama22@gmail.com>
2024-10-16 23:44:14 +0300

    deploy edit

--- server/server.js
+++ server/server.js
@@ -57,4 +57,10 @@ app.use("/api/shop/review", shopReviewRouter);
 
 app.use("/api/common/feature", commonFeatureRouter);
 
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+})
+
 app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
