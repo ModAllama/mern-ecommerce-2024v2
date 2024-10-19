0690058291612319fa6a2aa34134483fb26a94eb
Mahmoud Allama <mahmoudallama22@gmail.com>
2024-10-19 21:22:23 +0300

    7

--- server/server.js
+++ server/server.js
@@ -27,20 +27,20 @@ mongoose
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(
-  cors({
-    origin: "https://mern-ecommerce-2024-client.vercel.app",
-    methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
-    ],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://mern-ecommerce-2024-client.vercel.app",
+//     methods: ["GET", "POST", "DELETE", "PUT"],
+//     allowedHeaders: [
+//       "Content-Type",
+//       "Authorization",
+//       "Cache-Control",
+//       "Expires",
+//       "Pragma",
+//     ],
+//     credentials: true,
+//   })
+// );
 
 app.use(cookieParser());
 app.use(express.json());
